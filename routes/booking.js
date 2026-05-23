fc1038459ac814f5b351889d2ca8558f574feae9
Jashwanthkashaboina <jashwanthkashaboina02@gmail.com>
2026-05-23 16:12:24 +0530

    Add backend verification for Razorpay payments

--- routes/booking.js
+++ routes/booking.js
@@ -3,7 +3,7 @@ const router = express.Router({ mergeParams: true });
 const Booking = require('../models/booking');
 const Listing = require('../models/listing');
 const razorpay = require('../utils/razorpay');
-
+const crypto = require('crypto');
 
 router.post('/', async(req, res) =>{
     try{
@@ -96,6 +96,56 @@ router.post('/:bookingId/create-order', async(req, res) =>{
         console.error(err);
         res.status(500).json({ message: "Something went wrong" });
     }
-})
+});
+
+
+router.post('/verify-payment', async(req, res) => {
+    try{
+        const { 
+            razorpay_order_id,
+            razorpay_payment_id,
+            razorpay_signature,
+            bookingId
+         } = req.body;
+
+         const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+         const expectedSign = crypto
+                                .createHmac('sha256', process.env.RAZORPAY_KEY_SECRET)
+                                .update(body.toString())
+                                .digest('hex');
+
+        
+        if(expectedSign === razorpay_signature){
+            const booking = await Booking.findById(bookingId);
+
+            booking.status = 'confirmed';
+            booking.paymentId = razorpay_payment_id;
+            booking.orderId = razorpay_order_id;
+
+            await booking.save();
+
+            req.flash("success", "Payment Verified Successfully!");
+
+            return res.json({ 
+                success: true,
+                message: 'Payment verified'
+            });
+             
+        }
+
+        res.status(400).json({
+            success: false,
+            message: 'Invalid Signature',
+        });
+
+    } catch(err) {
+        console.error('Backend Payment verification Error : ', err);
+        res.status(500).json({
+            success: false,
+            message: "Verification Failed",
+        });
+    }
+});
 
 module.exports = router;
