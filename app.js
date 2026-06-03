c2fee02b6b8d0f59f5c28150ec3b968bea623db0
Jashwanthkashaboina <jashwanthkashaboina02@gmail.com>
2026-06-03 22:16:07 +0530

    Add google callback url in google strategy

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ passport.use(
     new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback'
+        callbackURL: process.env.GOOGLE_CALLBACK_URL
     },
     async(accessToken, refreshToken, profile, cb) =>{
         try{
