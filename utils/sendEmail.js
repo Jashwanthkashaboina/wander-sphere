cc09546a72d2bff300b9902693e93c0d9ceb1a07
Jashwanthkashaboina <jashwanthkashaboina02@gmail.com>
2026-06-04 11:57:33 +0530

    fix: update SMTP configuration for production

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,17 +2,21 @@ const nodemailer = require("nodemailer");
 // require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-
-    service: "gmail",
-
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
-
         user: process.env.EMAIL_USER,
-
         pass: process.env.EMAIL_PASSWORD,
-
     },
+});
 
+transporter.verify((err, success) => {
+    if(err){
+        console.log("SMTP ERROR:", err);
+    } else {
+        console.log("SMTP READY");
+    }
 });
 
 
